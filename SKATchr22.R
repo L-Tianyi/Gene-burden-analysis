8f6b4de9d4205e27233277cb6da4fe3dcb2bdfc0
L-Tianyi <78423195+L-Tianyi@users.noreply.github.com>
2021-08-18 00:39:45 +0100

    Update SKATchr22.R

--- SKATchr22.R
+++ SKATchr22.R
@@ -1,8 +1,9 @@
 library(data.table)
 chr22 <- as.data.frame(fread("~/NAS/joint_genotyping_anno_chr22.csv"))
 
-
+#############################
 ###investigate the data set##
+#############################
 cadd <- ggplot(chr22, aes(x = cadd_phred, y = AF)) + geom_point(color = "blue") + labs(x="CADD Score", y = "Allele frequency")
 cadd
 dann <- ggplot(chr22, aes(x = dann, y = AF)) + geom_point(color = "red") + labs(x="DANN Score", y = "Allele frequency")
@@ -13,8 +14,9 @@ table(Genotype$most_severe_consequence)
 genotype_rm <- Genotype[chr22$most_severe_consequence != "?",c(21,33)]
 box <- ggplot(genotype_rm, aes(x=most_severe_consequence, y = cadd_phred, color = most_severe_consequence)) + geom_boxplot()+ labs(x = "consequences", y = "CADD Score")
 
-
+#################
 ###subset data###
+#################
 chr22 <- chr22[,c(7,29,34:ncol(chr22))] ##allele frequencies, gene symbols, and genotypes are kept##
 chrM <- as.matrix(chr22) ##convert into matrix##
 
